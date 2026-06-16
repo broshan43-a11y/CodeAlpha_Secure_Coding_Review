4db9bb05a26008c1068329dfb96bfeb462f1cbbf
broshan43-a11y <broshan43@jnn.edu.in>
2026-06-16 11:08:15 +0530

    Create  vulnerable_login.py

---
+++ vulnerable_login.py
@@ -0,0 +1,7 @@
+username = input("Username: ")
+password = input("Password: ")
+
+query = "SELECT * FROM users WHERE username='" + username + "' AND password='" + password + "'"
+
+print("Executing Query:")
+print(query)
